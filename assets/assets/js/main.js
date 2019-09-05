5abd4a3d62dac9942279609d68b32ef3ad1b2c2c
Pallavi V G <pallavivg98@gmail.com>
2019-09-05 20:21:21 +0530

    Initial Commit

---
+++ assets/assets/js/main.js
@@ -0,0 +1,195 @@
+'use strict';
+$(window).load(function() {
+    $('.loader').delay(500).fadeOut('slow'); 
+});
+
+$('.team-image').addClass('slideanim')
+$('.team-name').addClass('slideanim')
+$('.list-inline.social-buttons').addClass('slideanim')
+$('.text-muted').addClass('slideanim')
+$('.counter').addClass('slideanim')
+$('a[href^="#"]').on('click', function(event) {
+    var target = $(this.getAttribute('href'));
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top
+        }, 1000);
+    }
+});
+
+$(document).ready(function(){
+    
+})
+
+$(document).ready(function() {
+    $(".dropdown-toggle").dropdown();
+});
+$(document).ready(function() {
+
+    'use strict';
+    /* ==============================================
+     /*   wow
+      =============================================== */
+    var wow = new WOW(
+        {
+            animateClass: 'animated',
+            offset: 10,
+            mobile: true
+        }
+    );
+    wow.init();
+    /* ==============================================
+        STICKY HEADER
+        =============================================== */
+
+    $(window).on('scroll', function () {
+        if ($(window).scrollTop() < 100) {
+            $('.header').removeClass('sticky_header');
+            $('.navbar-heading').addClass('d-none');
+        } else {
+            $('.header').addClass('sticky_header');
+            $('.navbar-heading').removeClass('d-none');
+        }
+    });
+   // Counter JS starts
+     function makeTimer() {
+
+        var endTime = new Date("22 September 2018 00:00:00 GMT+05:30");			
+        endTime = (Date.parse(endTime) / 1000);
+
+        var now = new Date();
+        now = (Date.parse(now) / 1000);
+
+        var timeLeft = endTime - now;
+
+        var days = Math.floor(timeLeft / 86400); 
+        var hours = Math.floor((timeLeft - (days * 86400)) / 3600);
+        var minutes = Math.floor((timeLeft - (days * 86400) - (hours * 3600 )) / 60);
+        var seconds = Math.floor((timeLeft - (days * 86400) - (hours * 3600) - (minutes * 60)));
+
+        if (hours < "10") { hours = "0" + hours; }
+        if (minutes < "10") { minutes = "0" + minutes; }
+        if (seconds < "10") { seconds = "0" + seconds; }
+
+        $("#days").html(days+"<small><small><small> D</small></small></small>");
+        $("#hours").html(hours+"<small><small><small> H</small></small></small>");
+        $("#minutes").html(minutes+"<small><small><small> M</small></small></small>");
+        $("#seconds").html(seconds+"<small><small><small> S</small></small></small>");		
+        
+}
+
+setInterval(function() { makeTimer(); }, 1000);
+    //Counter JS ends 
+
+    (function(){    
+        var link = document.getElementById("committee-nav");    
+        function checkWidth() {
+            var width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth,
+                newHref;
+            if (width<1300) {
+              newHref="#committee-mobile";
+            } else {
+              newHref="#committees";
+            } 
+           link.href=newHref;       
+        }
+        link.onclick = checkWidth;
+        checkWidth();
+    })();
+
+    /* ==============================================
+     SLIDER
+     =============================================== */
+    $(".cover_slider").owlCarousel({
+        loop:true,
+        autoplay:true,
+        smartSpeed:1000,
+        autoplayHoverPause:false,
+        dots:true,
+        nav:false,
+        items:1,
+        animateOut: 'fadeOut',
+        animateIn: 'fadeIn',
+        dotsContainer: '.cover_dots'
+    });
+
+    $(".brand_carousel").owlCarousel({
+        loop:true,
+        autoplay:false,
+        smartSpeed:450,
+        autoplayHoverPause:false,
+        dots:false,
+        nav:false,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:2
+            },
+            600:{
+                items:3
+
+            },
+            1000:{
+                items:4
+
+            }
+        },
+        items:5
+    });
+    /* ------------------------------------- */
+    /* Animated progress bars
+     /* ------------------------------------- */
+
+    var waypoints = $('.progress_container').waypoint(function() {
+        $('.progress .progress-bar').progressbar({
+            transition_delay: 1000
+        });
+    },{
+        offset: '50%'
+    });
+
+
+        /* --------------------------------------------------------
+    MAPS
+    ----------------------------------------------------------- */
+    var map = $('#map');
+    if(map.length > 0) {
+        google.maps.event.addDomListener(window, 'load', init);
+        var lattuide = map.attr('data-lat');
+        var longtuided = map.attr('data-lon');
+    }
+    function init() {
+        // Basic options for a simple Google Map
+        // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
+        var mapOptions = {
+            // How zoomed in you want the map to start at (always required)
+            zoom: 16,
+            scrollwheel: false,
+            navigationControl: false,
+            mapTypeControl: false,
+            scaleControl: false,
+            // The latitude and longitude to center the map (always required)
+            center: new google.maps.LatLng(lattuide, longtuided), // New York
+
+            // How you would like to style the map.
+            // This is where you would paste any style found on Snazzy Maps.
+            styles: [{"featureType":"water","stylers":[{"saturation":43},{"lightness":-11},{"hue":"#0088ff"}]},{"featureType":"road","elementType":"geometry.fill","stylers":[{"hue":"#ff0000"},{"saturation":-100},{"lightness":99}]},{"featureType":"road","elementType":"geometry.stroke","stylers":[{"color":"#808080"},{"lightness":54}]},{"featureType":"landscape.man_made","elementType":"geometry.fill","stylers":[{"color":"#ece2d9"}]},{"featureType":"poi.park","elementType":"geometry.fill","stylers":[{"color":"#ccdca1"}]},{"featureType":"road","elementType":"labels.text.fill","stylers":[{"color":"#767676"}]},{"featureType":"road","elementType":"labels.text.stroke","stylers":[{"color":"#ffffff"}]},{"featureType":"poi","stylers":[{"visibility":"off"}]},{"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#b8cb93"}]},{"featureType":"poi.park","stylers":[{"visibility":"on"}]},{"featureType":"poi.sports_complex","stylers":[{"visibility":"on"}]},{"featureType":"poi.medical","stylers":[{"visibility":"on"}]},{"featureType":"poi.business","stylers":[{"visibility":"simplified"}]}]
+        };
+
+        // Get the HTML DOM element that will contain your map
+        // We are using a div with id="map" seen below in the <body>
+        var mapElement = document.getElementById('map');
+
+        // Create the Google Map using our element and options defined above
+        var map = new google.maps.Map(mapElement, mapOptions);
+
+        // Let's also add a marker while we're at it
+        var marker = new google.maps.Marker({
+            position: new google.maps.LatLng(lattuide, longtuided),
+            map: map,
+            title: 'evento!'
+        });
+    }
+
+});
